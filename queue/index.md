e6151e804c6f3e7f1db62427ea3853f59a7e3a7a
Daniel JS <daniel@mydeveloperjourney.com>
2022-03-28 15:49:52 -0500

    wip: implements queue

---
+++ queue/index.md
@@ -0,0 +1,4 @@
+# Queue
+
+They are flexible and are also flexible in regards to their size, they are also very similar to a stack, the key difference is the way how data is removed from them, a queue uses a first in first out approach or FIFO approach. One way to visualize a queue is to imagine a line of people waiting to purchase their groceries at a grocery store. The people who get into line first are the people who get served at the checkout stand first.
+
---
+++ queue/script.js
@@ -0,0 +1,29 @@
+class Queue {
+    constructor() {
+        this.collection = [];
+    }
+
+    print() {
+        console.log(this.collection);
+    }
+
+    enqueue(element) {
+        this.collection.push(element);
+    }
+
+    dequeue() {
+        return this.collection.shift();
+    }
+
+    front() {
+        return this.collection[0];
+    }
+
+    size() {
+        return this.collection.length;
+    }
+
+    isEmpty() {
+        return !!this.collection.length
+    }
+}
